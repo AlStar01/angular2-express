b2a462ffbdbae533ca1336786662714b8be4d017
Alex Staroselsky <alexstaroselsky@gmail.com>
2016-09-03 12:38:43 -0500

    Created get all categories api route. Updated products route to include basic error handling

--- api/categories/categories.js
+++ api/categories/categories.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 let db = require('../../config/db');
 
 router.get('/', (req, res) => {
     db.getConnection((err, connection) => {
-        connection.query('SELECT * FROM product', (err, rows) => {
-            if(err) res.status(500).send('Error retrieving products');
+        connection.query('SELECT * FROM category', (err, rows) => {
+            if(err) res.status(500).send("Error retrieving categories");
 
             connection.release();
             res.send(rows);
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var products = require('./api/products/products');
+var categories = require('./api/categories/categories');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/api/products', products);
+app.use('/api/categories', categories);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
