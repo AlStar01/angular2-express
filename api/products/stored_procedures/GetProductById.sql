861564955423f6b0ebffe8d9ef8deacffe2c9602
Alex Staroselsky <alexstaroselsky@gmail.com>
2016-11-12 18:19:37 -0700

    Updated get product by id stored procedure to return quantity of respective product returned.

--- api/products/stored_procedures/GetProductById.sql
+++ api/products/stored_procedures/GetProductById.sql
@@ -3,24 +3,31 @@ DROP procedure IF EXISTS `GetProductById`;
 
 DELIMITER $$
 USE `sample`$$
-CREATE PROCEDURE `GetProductById` (IN productId BIGINT(20))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetProductById`(IN productId BIGINT(20))
 BEGIN
-	SELECT DISTINCT
-		p.product_id as productId,
-        p.name,
-        p.description,
-        p.model,
-        p.price,
-        p.featured_image as featuredImage,
-        c.category_id as categoryId,
-        c.name as categoryName,
-        c.description as categoryDescription,
-        COUNT(p.product_id) as quantity
+	CREATE TEMPORARY TABLE IF NOT EXISTS temp_product AS (
+		SELECT DISTINCT
+			p.product_id as productId,
+			p.name,
+			p.description,
+			p.model,
+			p.price,
+			p.featured_image as featuredImage,
+			c.category_id as categoryId,
+			c.name as categoryName,
+			c.description as categoryDescription
+		FROM
+			product p
+		JOIN 
+			category c ON c.category_id = p.category_id
+		WHERE p.product_id = productId
+	);
+    
+    SELECT
+		*,
+        (SELECT COUNT(product_id) FROM product WHERE model = temp_product.model) as quantity
 	FROM
-		product p
-	JOIN 
-		category c ON c.category_id = p.category_id
-	WHERE p.product_id = productId;
+		temp_product;
 END$$
 
 DELIMITER ;
