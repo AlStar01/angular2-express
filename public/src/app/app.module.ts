ae52eeaff504a137a12d6df8603a6dec792bae19
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-03-10 11:56:42 -0700

    Created basic about component that will be used to demonstrate project technologies.

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
