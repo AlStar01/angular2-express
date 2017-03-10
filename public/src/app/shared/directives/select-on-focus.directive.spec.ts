d25af556fb52f1bb10456933ba553845229212d9
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-03-10 12:04:20 -0700

    Created shell for select on focus directive.

---
+++ public/src/app/shared/directives/select-on-focus.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SelectOnFocusDirective } from './select-on-focus.directive';
+
+describe('SelectOnFocusDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SelectOnFocusDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- public/src/app/shared/shared.module.ts
+++ public/src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectOnFocusDirective } from './directives/select-on-focus.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [
-    CommonModule
+    CommonModule,
+    SelectOnFocusDirective
   ],
-  declarations: []
+  declarations: [SelectOnFocusDirective]
 })
 export class SharedModule { }
