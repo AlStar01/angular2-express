9a913c0fd5f452d30c3556e455b8692e87295e94
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-03-10 14:36:57 -0700

    Added functionality to select on focus directive to select all text within an input element on click.

--- public/src/app/shared/directives/select-on-focus.directive.ts
+++ public/src/app/shared/directives/select-on-focus.directive.ts
@@ -1,10 +1,17 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[appSelectOnFocus]'
 })
 export class SelectOnFocusDirective {
 
-  constructor(private ref: ElementRef) { }
+  constructor(private el: ElementRef, private renderer: Renderer) { }
 
+  @HostListener('click') onclick() {
+    this.select();
+  }
+
+  private select() {
+    this.renderer.invokeElementMethod(this.el.nativeElement, 'select');
+  }
 }
