6f961d3c0a41fc42354ca42ea7f2284affe3124f
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-03 22:37:06 -0600

    Created stub for category detail component

--- public/src/app/categories/categories.module.ts
+++ public/src/app/categories/categories.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryDetailComponent } from './category-detail/category-detail.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CategoryListComponent]
+  declarations: [CategoryListComponent, CategoryDetailComponent]
 })
 export class CategoriesModule { }
