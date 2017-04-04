48a084beae2cab25985bf163dd889b63d90d0e77
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-03 22:43:59 -0600

    Created stub for category service

--- public/src/app/categories/categories.component.ts
+++ public/src/app/categories/categories.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { CategoryService } from "app/categories/category.service";
 
 @Component({
-    template: '<router-outlet></router-outlet>'
+    template: '<router-outlet></router-outlet>',
+    providers: [CategoryService]
 })
 export class CategoriesComponent {}
