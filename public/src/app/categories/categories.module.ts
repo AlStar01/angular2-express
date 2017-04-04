aa2de4431828d8d8bf0200b3217c7cc3292f2cbd
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-03 22:36:36 -0600

    Created stub for category list component

---
+++ public/src/app/categories/categories.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoryListComponent } from './category-list/category-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CategoryListComponent]
+})
+export class CategoriesModule { }
