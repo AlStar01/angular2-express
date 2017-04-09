87afe928cec6550ec91137e4d0e755746aca35d7
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-09 14:54:04 -0600

    Updated category list modal to use component modal rather than inline ng-template/template.

--- public/src/app/categories/shared/category-add/category-add.component.ts
+++ public/src/app/categories/shared/category-add/category-add.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-category-add',
   templateUrl: './category-add.component.html',
   styleUrls: ['./category-add.component.css']
 })
-export class CategoryAddComponent implements OnInit {
+export class CategoryAddComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(public activeModal: NgbActiveModal) { }
 }
