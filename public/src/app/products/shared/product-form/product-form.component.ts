885da8ce58e0825870014f867faf017d22f4b50d
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-25 20:38:25 -0600

    Added reactive form fields to product form component. Added metadata for product @Input.

--- public/src/app/products/shared/product-form/product-form.component.ts
+++ public/src/app/products/shared/product-form/product-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Product } from "../../product";
 
 @Component({
   selector: 'app-product-form',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
-
-  constructor() { }
+  @Input() product: Product;
+  
+  private productForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
   }
 
+  private createForm() {
+    this.productForm = this.fb.group({
+      name: [ '', Validators.required ],
+      description: [ '', Validators.required ],
+      price: [ '', Validators.required ],
+      color: [ '', Validators.required ],
+      material: [ '', Validators.required ],
+      origin: [ '', Validators.required ],
+      manufacturer: [ '', Validators.required ],
+      featured_image: [ '', Validators.required ],
+      category_id: [ '', Validators.required ],
+
+    });
+  }
 }
