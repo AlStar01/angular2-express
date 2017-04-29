fb3447cfa3896e7a57bf2dc3b9504df88749c7dd
Alex Staroselsky <alexstaroselsky@gmail.com>
2017-04-29 13:50:45 -0600

    Updated express server to resolve path to dist folder of angular-cli build command. Added socket.io to express server api.

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'dist')));
 
 app.use(api);
-app.use('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.use('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'dist', 'index.html')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
